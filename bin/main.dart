0fee0b86a28e5713412a41eaa42f72ba1d23e3b3
Klemen Tušar <techouse@gmail.com>
2022-03-13 22:26:19 +0000

    :recycle: Refactor caching to AlfredWorkflow

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io' show Platform, exitCode, stdout;
+import 'dart:io' show exitCode, stdout;
 
 import 'package:alfred_workflow/alfred_workflow.dart'
     show
@@ -9,40 +9,11 @@ import 'package:alfred_workflow/alfred_workflow.dart'
         AlfredWorkflow;
 import 'package:algolia/algolia.dart' show AlgoliaQuerySnapshot;
 import 'package:args/args.dart' show ArgParser, ArgResults;
-import 'package:path/path.dart' show dirname;
-import 'package:stash/stash_api.dart'
-    show
-        Cache,
-        CacheEntryCreatedEvent,
-        CacheEntryEvictedEvent,
-        CacheEntryExpiredEvent,
-        CacheEntryRemovedEvent,
-        CacheEntryUpdatedEvent,
-        CreatedExpiryPolicy,
-        EventListenerMode,
-        LruEvictionPolicy,
-        CacheExtension;
-import 'package:stash_file/stash_file.dart'
-    show FileCacheStore, newFileLocalCacheStore;
 
-import 'src/extensions/string_helpers.dart' show StringHelpers;
 import 'src/models/search_result.dart' show SearchResult;
 import 'src/services/algolia_search.dart' show AlgoliaSearch;
 
 final AlfredWorkflow workflow = AlfredWorkflow();
-
-final FileCacheStore store = newFileLocalCacheStore(
-  path: dirname(Platform.script.toFilePath()),
-  fromEncodable: (Map<String, dynamic> json) => AlfredItems.fromJson(json),
-);
-
-final Cache cache = store.cache<AlfredItems>(
-  name: 'query_cache',
-  maxEntries: 10,
-  eventListenerMode: EventListenerMode.synchronous,
-  evictionPolicy: const LruEvictionPolicy(),
-  expiryPolicy: const CreatedExpiryPolicy(Duration(minutes: 1)),
-);
 
 bool verbose = false;
 
@@ -61,18 +32,13 @@ void main(List<String> arguments) async {
 
     final String query = args['query'].replaceAll(RegExp(r'\s+'), ' ').trim();
 
-    if (verbose) {
-      stdout.writeln('Query: "$query"');
-      _cacheVerbosity();
-    }
+    if (verbose) stdout.writeln('Query: "$query"');
 
     if (query.isEmpty) {
       _showPlaceholder();
     } else {
-      final AlfredItems? cachedItem = await cache.get(query.md5hex);
-      if (cachedItem != null) {
-        workflow.addItems(cachedItem.items);
-      } else {
+      workflow.cacheKey = query;
+      if (await workflow.getItems() == null) {
         await _performSearch(query);
       }
     }
@@ -90,20 +56,6 @@ void main(List<String> arguments) async {
   }
 }
 
-void _cacheVerbosity() {
-  cache
-    ..on<CacheEntryCreatedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" added'))
-    ..on<CacheEntryUpdatedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.newEntry.key}" updated'))
-    ..on<CacheEntryRemovedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" removed'))
-    ..on<CacheEntryExpiredEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" expired'))
-    ..on<CacheEntryEvictedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" evicted'));
-}
-
 void _showPlaceholder() {
   workflow.addItem(
     const AlfredItem(
@@ -118,7 +70,7 @@ Future<void> _performSearch(String query) async {
 
   if (snapshot.nbHits > 0) {
     final AlfredItems items = AlfredItems(
-      items: snapshot.hits
+      snapshot.hits
           .map(
             (snapshot) => SearchResult.fromJson(snapshot.data),
           )
@@ -141,7 +93,6 @@ Future<void> _performSearch(String query) async {
           )
           .toList(),
     );
-    cache.putIfAbsent(query.md5hex, items);
     workflow.addItems(items.items);
   } else {
     final Uri url = Uri.https(
